85fdc773d5f26952eb8ab1ab17103ff15bee3211
duc1612112@gmail.com <KHTNduc768319#>
2020-05-06 15:31:02 +0700

    Project init

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Hello, teacher!</Text>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
